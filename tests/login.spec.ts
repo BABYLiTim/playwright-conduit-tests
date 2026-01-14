6ff4a027f2d3cdd6027689e7e3332031fc49d1ae
Viktor Timanov <manualqaviktor@gmail.com>
2026-01-14 16:17:36 +0100

    Add login UI test (invalid credentials)

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -17,3 +17,18 @@ test('User logs in with valid credentials', async({page}) => {
     await expect(homeLink).toBeVisible()
     await expect(userName).toBeVisible()
 })
+
+test('User logs in with invalid email', async({page}) => {
+    const inValidEmail = process.env.USER_EMAIL_INVALID!
+    const validPassword = process.env.USER_PASSWORD_VALID!
+
+    await page.goto('/')
+    await page.getByRole('link', {name: 'Sign in'}).click()
+    await page.getByRole('textbox', {name: 'Email'}).fill(inValidEmail)
+    await page.getByRole('textbox', {name: 'Password'}).fill(validPassword)
+    await page.getByRole('button', {name: 'Sign in'}).click()
+
+    const errorMessage = page.getByText('email or password is invalid')
+
+    await expect(errorMessage).toBeVisible()
+})
