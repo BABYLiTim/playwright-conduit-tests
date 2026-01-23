a4cc49677620899a873a75e8588d371282c08cf1
Viktor Timanov <manualqaviktor@gmail.com>
2026-01-23 18:06:49 +0100

    Add the Settings PO and Logout test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,29 +1,48 @@
 import { test, expect } from '@playwright/test';
 import { PageManager } from '../pages/pageManager'
 
+
 test('User logs in with valid credentials', async({page}) => {
-    const pageManager = new PageManager(page)
-    const loginPage = pageManager.navigateToLoginPage()
-    const validEmail = process.env.USER_EMAIL_VALID!
-    const validPassword = process.env.USER_PASSWORD_VALID!
+        const pageManager = new PageManager(page)
+        const loginPage = pageManager.getLoginPage()
+        const validEmail = process.env.USER_EMAIL_VALID!
+        const validPassword = process.env.USER_PASSWORD_VALID!
 
-    await loginPage.open()
-    await loginPage.login(validEmail, validPassword)
+        await loginPage.open()
+        await loginPage.login(validEmail, validPassword)
 
-    await expect(pageManager.onHomePage().home).toBeVisible()
-})
+        await expect(pageManager.getHomePage().home).toBeVisible()
+    })
 
 test('User logs in with invalid email', async({page}) => {
+        const pageManager = new PageManager(page)
+        const loginPage = pageManager.getLoginPage()
+        const inValidEmail = process.env.USER_EMAIL_INVALID!
+        const validPassword = process.env.USER_PASSWORD_VALID!
+
+        await loginPage.open()
+        await loginPage.login(inValidEmail, validPassword)
+
+        await expect(pageManager.getLoginPage().getErrorMessage()).toBeVisible()
+    })
+
+test('User can logout', async({page}) => {
     const pageManager = new PageManager(page)
-    const loginPage = pageManager.navigateToLoginPage()
-    const inValidEmail = process.env.USER_EMAIL_INVALID!
+    const loginPage = pageManager.getLoginPage()
+    const inValidEmail = process.env.USER_EMAIL_VALID!
     const validPassword = process.env.USER_PASSWORD_VALID!
 
     await loginPage.open()
     await loginPage.login(inValidEmail, validPassword)
 
-    await expect(pageManager.navigateToLoginPage().getErrorMessage()).toBeVisible()
-})
+    const settingsPage = pageManager.getSettingsPage()
+    await settingsPage.open()
+    await settingsPage.logout()
+
+    const signinButton = page.getByRole('link', {name: 'Sign in'})
+    await page.reload()
+    await expect(signinButton).toBeVisible()
+})    
 
 test('User can login via API and receive token', async ({request}) => {
     const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
